ab9b4f1876f8e1ef3625f721fc52a1835ee710df
soljinn <soljin97@gmail.com>
2022-12-06 08:16:16 +0700

    update

---
+++ BaiTap/ss6/ung_dung_may_tinh/ung_dung_may_tinh.js
@@ -0,0 +1,24 @@
+function add() {
+    let number1= document.getElementById('num1').value;
+    let number2= document.getElementById('num2').value;
+    let result= Number(number1)+ Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
+function sub() {
+    let number1= document.getElementById('num1').value;
+    let number2= document.getElementById('num2').value;
+    let result= Number(number1) - Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
+function multi() {
+    let number1= document.getElementById('num1').value;
+    let number2= document.getElementById('num2').value;
+    let result= Number(number1) * Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
+function divi() {
+    let number1= document.getElementById('num1').value;
+    let number2= document.getElementById('num2').value;
+    let result= Number(number1) / Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
