41b95b9d565d20957fa217cd6413ff7b3b826a79
soljinn <soljin97@gmail.com>
2022-12-08 11:43:03 +0700

    update

--- BaiTap/ss7/ung_dung_chuyen_doi_tien_te/chuyen_doi_tien_te.js
+++ BaiTap/ss7/ung_dung_chuyen_doi_tien_te/chuyen_doi_tien_te.js
@@ -1,15 +1,15 @@
 function ChangeMoney() {
     let amount = document.getElementById("amount").value;
-    let fromC = document.getElementById("from").value;
+    let formC = document.getElementById("from").value;
     let to = document.getElementById("to").value;
     let result='';
-    if (fromC == "USD" && to == "VND") {
+    if (formC == "USD" && to == "VND") {
         result = "result: " + (amount * 23000) + " Đ";
     }
-    else if (fromC == "VND"&& To =="USD"){
+    else if (formC == "VND"&& To =="USD"){
         result = "result: "+ (amount / 23000) + " $";
     }
-    else if (fromC == "VND") {
+    else if (formC == "VND") {
         result = "result: " + amount + " Đ";
     } else {
         result = "result: " + amount + " $";
